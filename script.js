e0ce4522b1959cbf96ec4165cd252bc09e08ce72
Rahul Pardeshi <165809864+rahulpardeshi86@users.noreply.github.com>
2026-01-11 10:24:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var menubar=document.querySelector("#menu-bars");
+var navbar= document.querySelector(".navbar");
+
+menubar.onclick=()=>{
+    menubar.classList.toggle('fa-time');
+    navbar.classList.toggle('active');
+
+}
+// Appointement
